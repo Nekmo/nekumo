8944ec438a8248dd9794f7263415540465dbb9f3
Nekmo <contacto@nekmo.com>
2016-01-16 03:38:23 +0100

    Css minificado y js preparado para Angular.

--- nekumo/servers/web/Gruntfile.js
+++ nekumo/servers/web/Gruntfile.js
@@ -1,31 +1,92 @@
 const fs = require('fs');
 
-function readAsserts(file, regex){
-    var text = fs.readFileSync(file, 'utf-8');
-    return regex.exec(file);
+function getMatches(string, regex, index) {
+    index || (index = 1); // default to the first capturing group
+    var matches = [];
+    var match;
+    while (match = regex.exec(string)) {
+        matches.push(match[index]);
+    }
+    return matches;
 }
-// TODO: no funciona
-console.log(readAsserts('templates/web/node.html', new RegExp('"{{ nekumo_root }}/([^"]+)\.js"', 'gi')));
+
+function readAsserts(file, regex){
+    regex = new RegExp(regex, 'gi');
+    var text = fs.readFileSync(file, 'utf-8');
+    var matches = getMatches(text, regex);
+    console.log('Parsing asserts from *' + file + '*:');
+    matches.forEach(function(x){
+        console.log('  - ' + x);
+    });
+    return matches;
+}
+
 module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
+        ngAnnotate: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
             },
             build: {
-                src: readAsserts('templates/web/node.html', new RegExp('"{{ nekumo_root }}/([^"]+)\.js"')),
-                dest: 'build/<%= pkg.name %>.min.js'
+                files: [
+                    {
+                        src: readAsserts('templates/web/node.html', '"{{ nekumo_root' + ' }}/([^"}]+\.js)"'),
+                        dest: '/tmp/_nekumoBuild/node.js'
+                    },
+                    {
+                        src: readAsserts('templates/web/base.html', '"{{ nekumo_root }}/([^"}]+\.js)"'),
+                        dest: '/tmp/_nekumoBuild/base.js'
+                    }
+                ]
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
+            },
+            build: {
+                files: [
+                    {
+                        src: '/tmp/_nekumoBuild/node.js',
+                        dest: 'static/build/node.min.js'
+                    },
+                    {
+                        src: '/tmp/_nekumoBuild/base.js',
+                        dest: 'static/build/base.min.js'
+                    }
+                ]
+            }
+        },
+        cssmin: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd HH:MM") %> */\n'
+            },
+            build: {
+                files: [
+                    {
+                        src: readAsserts('templates/web/node.html', '"{{ nekumo_root' + ' }}/([^"}]+\.css)"'),
+                        dest: 'static/build/node.min.css'
+                    },
+                    {
+                        src: readAsserts('templates/web/base.html', '"{{ nekumo_root }}/([^"}]+\.css)"'),
+                        dest: 'static/build/base.min.css'
+                    }
+                ]
             }
         }
     });
 
+    // Load the plugin that provides the "grunt-ng-annotate" task.
+    grunt.loadNpmTasks('grunt-ng-annotate');
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "cssmin" task.
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['ngAnnotate', 'uglify', 'cssmin']);
 
 };
